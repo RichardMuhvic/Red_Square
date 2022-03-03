be711dff202e8440a943ad39e591341622fe1778
Richard <richardmuhvic@gmail.com>
2022-03-03 17:34:57 +0100

    j'ajoute un effet de fade in sur des éléments au scroll

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@
 document.getElementsByClassName('work-anim').addEventListener('mouseover', function() {
     backgroundBlur(`filter=blur(${10}px)`);
 })*/
+
+
+const items = document.querySelectorAll('.appear2');
+
+const active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add('inview2'); 
+        }else{
+            entry.target.classList.remove('inview2'); 
+        }
+    });
+}
+
+const io2 = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
