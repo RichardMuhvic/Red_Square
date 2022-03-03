05bdc9d6f96fe1d5e808a9203570c49cff69058f
Richard <richardmuhvic@gmail.com>
2022-03-03 13:53:32 +0100

    slider

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*function backgroundBlur(filter) {
+    document.body.style.background = filter; 
+}
+
+document.getElementsByClassName('work-anim').addEventListener('mouseover', function() {
+    backgroundBlur(`filter=blur(${10}px)`);
+})*/
