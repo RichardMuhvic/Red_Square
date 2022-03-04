a7c2e67758bac49efec245bc4f4284ac2363e652
Richard <richardmuhvic@gmail.com>
2022-03-04 14:08:26 +0100

    je travaille sur le flou total de body totu en gardant le 'work' net

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-/*function backgroundBlur(filter) {
-    document.body.style.background = filter; 
-}
 
-document.getElementsByClassName('work-anim').addEventListener('mouseover', function() {
-    backgroundBlur(`filter=blur(${10}px)`);
-})*/
+//Blur du body au passage de la souris sur "work" :
+let buttonWork = document.querySelector(".work-anim");
+
+//la souris sur 'work' totu est flou:
+buttonWork.addEventListener("mouseover", function() {
+    document.getElementById("blur").style.filter = "blur(10px)";
+})
+//la souris en-dehors de 'work', tout est net:
+buttonWork.addEventListener("mouseout", function() {
+    document.getElementById("blur").style.filter = "none";
+})
+//garder le bouton "work" net avec le fond flouté:
 
 
+
+
+
+
+//Création du fade au scroll :
 const items = document.querySelectorAll('.appear2');
 
 const active = function(entries){
@@ -23,5 +34,4 @@ const io2 = new IntersectionObserver(active);
  for(let i=0; i < items.length; i++){
     io2.observe(items[i]);
  }
-
 
